1507c38a8bf3468ae38ab2b65fd116b0931262be
Denis3432522 <denis_lukyanov_03@mail.ru>
2023-11-26 21:11:14 +1000

    0.0.1 - SNAPSHOT

---
+++ src/main/java/org/aviatrip/representativeservice/RepresentativeServiceApplication.java
@@ -0,0 +1,14 @@
+package org.aviatrip.representativeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class RepresentativeServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RepresentativeServiceApplication.class, args);
+    }
+}
+
